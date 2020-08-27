ce329df91fbaa88a634933398244d411972c8dc3
Yuehan Huang <yuehan.huang@protonmail.com>
2020-08-27 05:58:51 -0700

    Create basic rail database with person and housse as models

--- W4/W4D4/rspec_project/lib/dessert.rb
+++ W4/W4D4/rspec_project/lib/dessert.rb
@@ -9,39 +9,41 @@ require 'active_support/inflector'
 # See https://api.rubyonrails.org/classes/ActiveSupport/Inflector.html#method-i-pluralize
 
 class Dessert
-  attr_reader :type, :quantity, :ingredients, :temp
+    attr_reader :type, :quantity, :ingredients, :temp
 
-  def initialize(type, quantity, chef)
-    raise ArgumentError unless quantity.is_a?(Integer)
-    @type = type
-    @quantity = quantity
-    @chef = chef
-    @ingredients = []
-    @temp = 60
-  end
+    def initialize(type, quantity, chef)
+        raise ArgumentError unless quantity.is_a?(Integer)
 
-  def add_ingredient(ingredient)
-    @ingredients << ingredient
-  end
+        @type = type
+        @quantity = quantity
+        @chef = chef
+        @ingredients = []
+        @temp = 60
+    end
 
-  def mix!
-    @ingredients.shuffle!
-  end
+    def add_ingredient(ingredient)
+        @ingredients << ingredient
+    end
 
-  def heat!
-    @temp = 400
-  end
+    def mix!
+        @ingredients.shuffle!
+    end
 
-  def eat(amount)
-    raise "not enough left!" if @quantity - amount < 0
-    @quantity -= amount
-  end
+    def heat!
+        @temp = 400
+    end
 
-  def serve
-    "#{@chef.titleize} has made #{@quantity} #{@type.pluralize}!"
-  end
+    def eat(amount)
+        raise 'not enough left!' if @quantity - amount < 0
 
-  def make_more
-    @chef.bake(self)
-  end
+        @quantity -= amount
+    end
+
+    def serve
+        "#{@chef.titleize} has made #{@quantity} #{@type.pluralize}!"
+    end
+
+    def make_more
+        @chef.bake(self)
+    end
 end
