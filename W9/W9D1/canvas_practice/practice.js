5da52d9337e8759cd113bfec1dc8ceae8c49c796
Yuehan <yuehan.huang@protonmail.com>
2020-09-22 08:51:50 -0700

    Create canvas intro hw

--- W9/W9D1/canvas_practice/practice.js
+++ W9/W9D1/canvas_practice/practice.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
+    const canvas = document.getElementById("mycanvas")
+    canvas.height = 500;
+    canvas.width = 500;
 
+    const ctx = canvas.getContext("2d");
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+          ctx.fillStyle = `rgb(
+              ${Math.floor(255 - 42.5 * i)},
+              ${Math.floor(255 - 42.5 * j)},
+              0)`;
+          ctx.fillRect(j * 25, i * 25, 25, 25);
+        }
+      }
+
+    ctx.beginPath();
+    ctx.arc(200,200, 30,0, 2*Math.PI,true);
+    ctx.strokeStyle = "purple";
+    ctx.lineWidth = 10;
+    ctx.stroke();
+    ctx.fillStyle = "teal";
+    ctx.fill();
 });
